f5c6716d6dde011820cc3e7306d40b7d7f6cf02d
Eduardo Soto <esotom92@gmail.com>
2019-01-18 20:04:35 -0300

    + improve: followed tslint rules (except for no-parameter-reassignment)

--- src/SchemaGenerator.ts
+++ src/SchemaGenerator.ts
@@ -1,38 +1,38 @@
-import {Node,rowClassify} from "./headerClassifier";
+import { Node, rowClassify } from './headerClassifier';
 
-export function generateSchema(headerList:Array<string>):object {
-    let schema:object = {};
-    let nodeList:Array<Node> = [];
-    let headerSize:number = headerList.length;
-    for (let i:number = 0;i<headerSize;i++) {
-        nodeList = nodeList.concat(rowClassify(headerList[i],schema));
+export function generateSchema(headerList: string[]): object {
+  const schema: object = {};
+  let nodeList: Node[] = [];
+  const headerSize: number = headerList.length;
+  for (let i = 0; i < headerSize; i += 1) {
+    nodeList = nodeList.concat(rowClassify(headerList[i], schema));
+  }
+  const maxlvl: number = Math.max.apply(Math, nodeList.map((node: Node) => node.level));
+  const nodesMap: { [level: number]: Node[] } = {};
+  nodeList.forEach((node) => {
+    if (!nodesMap[node.level]) {
+      nodesMap[node.level] = [];
     }
-    let maxlvl:number = Math.max.apply(Math,nodeList.map((node:Node) => node.level));
-    const nodesMap: { [level: number]: Node[] } = {};
-    nodeList.forEach((node) => {
-        if (!nodesMap[node.level]) {
-             nodesMap[node.level] = [];
-        }
-        nodesMap[node.level].push(node);
-    } );
+    nodesMap[node.level].push(node);
+  });
 
-    for (let j:number = 1; j<=maxlvl; j++) {
-        let nodeListPerLevel:Array<Node> = nodesMap[j];
-        let pathTo:any = schema;
-        nodeListPerLevel.forEach(node => {
-            pathTo = schema;
-            for (let p:number = 0, parentSize:number = node.parent.length;p<parentSize;p++) {
-                pathTo = pathTo[node.parent[p]];
-            }
-            let key:(string | number) = null;
-            if (node.ptype === "Array") key = node.index;
-            else if (node.ptype === "Object") key = node.key;
-            if (key) {
-                if (node.isLeaf) pathTo[key] = "String";
-                else if (node.type === "Array")  pathTo[key] = [];
-                else if (node.type === "Object") pathTo[key] = {};   
-            }
-        });
-    }
-    return schema;
+  for (let j = 1; j <= maxlvl; j += 1) {
+    const nodeListPerLevel: Node[] = nodesMap[j];
+    let pathTo: any = schema;
+    nodeListPerLevel.forEach((node) => {
+      pathTo = schema;
+      for (let p = 0, parentSize: number = node.parent.length; p < parentSize; p += 1) {
+        pathTo = pathTo[node.parent[p]];
+      }
+      let key: (string | number) = null;
+      if (node.ptype === `Array`) key = node.index;
+      else if (node.ptype === `Object`) key = node.key;
+      if (key) {
+        if (node.isLeaf) pathTo[key] = `String`;
+        else if (node.type === `Array`)  pathTo[key] = [];
+        else if (node.type === `Object`) pathTo[key] = {};
+      }
+    });
+  }
+  return schema;
 }
--- src/tokenClassify.ts
+++ src/tokenClassify.ts
@@ -3,96 +3,95 @@
  * Recieves a row name, and process it to return an object with two arrays.
     * tokens: Array of keys or indexes (as strings) of the acces sequence to the row value
     * modes: Array of strings describing in an orderly fashion the nesting of the row value
- 
-* Example: 
-    let str:string = 'pescado{perro}[90]{oso}{casa}[11]';
 
-    let res:INestingMap = tokenizeClassifier(str);  
+* Example:
+    let str:string = `pescado{perro}[90]{oso}{casa}[11]`;
+
+    let res:INestingMap = tokenizeClassifier(str);
 
     return res;
 
     * returns: {
-        "tokens": [
-            "pescado",
-            "perro",
-            "90",
-            "oso",
-            "casa",
-            "11"
+        `tokens`: [
+            `pescado`,
+            `perro`,
+            `90`,
+            `oso`,
+            `casa`,
+            `11`
         ],
-        "modes": [
-            "Root",
-            "Object",
-            "Array",
-            "Object",
-            "Object",
-            "Array"
+        `modes`: [
+            `Root`,
+            `Object`,
+            `Array`,
+            `Object`,
+            `Object`,
+            `Array`
         ]
 }
  */
 
 export interface INestingMap {
-    tokens: Array<string>;
-    modes: Array<"Root" | "Object" | "Array">;
+  tokens: string[];
+  modes: (`Root` | `Object` | `Array`)[];
 }
 
-export function tokenizeClassifier(rowName:string):INestingMap {
-    let nestingMap:INestingMap = {tokens: [],modes: []};
-    let inMode:("Root" | "Object" | "Array") = "Root";
-    while(rowName !== "") {
-        nestingMap.modes.push(inMode);
-        let tokenizer:Array<string>;
-        switch (inMode) {
-            case "Root":
-                tokenizer = rowName.match(/\w+(?=(\[|\{))/);
-                if (tokenizer === null) {
-                    nestingMap.tokens.push(rowName);
-                    rowName = "";
-                    break;
-                }
-                else if (tokenizer.length !== 2) {
-                    throw "Input Error, header name generates bad reading. Cause: ${ rowName }";
-                }
-                if (tokenizer[0] != null) {
-                    nestingMap.tokens.push(tokenizer[0]);
-                }
-                if (tokenizer[1] === "[") {
-                    inMode = "Array";
-                }
-                else if (tokenizer[1] === "{") {
-                    inMode = "Object";
-                }
-                rowName = rowName.slice(tokenizer[0].length);
-                break;
-            case "Object":
-                tokenizer = rowName.match(/\w+(?=\}(\[|\{)?)/);
-                if (tokenizer[0] != null) {
-                    nestingMap.tokens.push(tokenizer[0]);
-                }
-                if (tokenizer[1] === "{") {
-                    inMode = "Object";
-                }
-                else if (tokenizer[1] === "[") {
-                    inMode = "Array";
-                }
-                rowName = rowName.slice(tokenizer[0].length+2); // length +2 includes the parenthesis
-                break;
-            case "Array":
-                tokenizer = rowName.match(/\w+(?=\](\{|\[)?)/);
-                if (tokenizer[0] !== null) {
-                    nestingMap.tokens.push(tokenizer[0]);
-                }
-                if (tokenizer[1] === "{") {
-                    inMode = "Object";
-                }
-                else if (tokenizer[1] === "[") {
-                    inMode = "Array";
-                }
-                rowName = rowName.slice(tokenizer[0].length+2); // length +2 includes the parenthesis
-                break;
-            default:
-                break;
+export function tokenizeClassifier(rowName: string): INestingMap {
+  const nestingMap: INestingMap = {
+    tokens: [],
+    modes: [],
+  };
+  let inMode: (`Root` | `Object` | `Array`) = `Root`;
+  while (rowName !== ``) {
+    nestingMap.modes.push(inMode);
+    let tokenizer: string[];
+    switch (inMode) {
+      case `Root`:
+        tokenizer = rowName.match(/\w+(?=(\[|\{))/);
+        if (tokenizer === null) {
+          nestingMap.tokens.push(rowName);
+          rowName = ``;
+          break;
+        } else if (tokenizer.length !== 2) {
+          throw `Input Error, header name generates bad reading. Cause: ${ rowName }`;
         }
+        if (tokenizer[0] != null) {
+          nestingMap.tokens.push(tokenizer[0]);
+        }
+        if (tokenizer[1] === `[`) {
+          inMode = `Array`;
+        } else if (tokenizer[1] === `{`) {
+          inMode = `Object`;
+        }
+        rowName = rowName.slice(tokenizer[0].length);
+        break;
+      case `Object`:
+        tokenizer = rowName.match(/\w+(?=\}(\[|\{)?)/);
+        if (tokenizer[0] != null) {
+          nestingMap.tokens.push(tokenizer[0]);
+        }
+        if (tokenizer[1] === `{`) {
+          inMode = `Object`;
+        } else if (tokenizer[1] === `[`) {
+          inMode = `Array`;
+        }
+        rowName = rowName.slice(tokenizer[0].length + 2); // length +2 includes the parenthesis
+        break;
+      case `Array`:
+        tokenizer = rowName.match(/\w+(?=\](\{|\[)?)/);
+        if (tokenizer[0] !== null) {
+          nestingMap.tokens.push(tokenizer[0]);
+        }
+        if (tokenizer[1] === `{`) {
+          inMode = `Object`;
+        } else if (tokenizer[1] === `[`) {
+          inMode = `Array`;
+        }
+        rowName = rowName.slice(tokenizer[0].length + 2); // length +2 includes the parenthesis
+        break;
+      default:
+        break;
     }
-    return nestingMap;
+  }
+  return nestingMap;
 }
