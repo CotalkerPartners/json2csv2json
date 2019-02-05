eeb49b1f47c9dbaff17b2e0ce96bdb6ea4d441be
Eduardo Soto <esotom92@gmail.com>
2019-02-05 19:28:41 -0300

    + feature: implemented transform stream. replaced 'csv-parse' with internal buffer handling

--- src/CSV2JSON.ts
+++ src/CSV2JSON.ts
@@ -2,7 +2,6 @@ import { Transform } from 'stream';
 import firstline = require('firstline');
 import { generateSchema } from './SchemaGenerator';
 import { csvDataToJSON } from './csvParser';
-const csv = require('csv-parser');
 
 interface IrowConfig {
   rowID: number;
@@ -24,8 +23,10 @@ export class CSV2JSON extends Transform {
   hasHeader: boolean;
   rows: IrowConfig[];
   schema: object;
+  remainder: string;
   constructor(headersOrCsvPath: string, config: IconfigObj, headerAsString: boolean) {
-    super({ writableObjectMode: true });
+    super({ writableObjectMode: true, objectMode: true });
+    this.remainder = '';
     if (config === undefined) {
       this.separator = ',';
       this.hasHeader = true;
@@ -38,7 +39,10 @@ export class CSV2JSON extends Transform {
     if (headerAsString && headersOrCsvPath !== undefined) {
       this.headerList = headersOrCsvPath.split(this.separator).map(h => h.trim());
     } else if (headersOrCsvPath !== undefined && this.hasHeader) {
-      this.headerList = firstline(headersOrCsvPath).split(this.separator).map(h => h.trim());
+      firstline(headersOrCsvPath)
+      .then((headerLine) => {
+        this.headerList = headerLine.split(this.separator).map(h => h.trim());
+      });
     }
 
     if (this.headerList !== [] && this.headerList !== undefined && this.rows === []) {
@@ -66,36 +70,47 @@ export class CSV2JSON extends Transform {
     this.schema = generateSchema(pathList);
     console.log(JSON.stringify(this.schema, null, 2));
   }
+
   // tslint:disable-next-line
-  _transform(data, encoding, callback) { 
-    this.on('pipe', (source) => {
-      source.unpipe(this);
-      source.pipe(csv({ separator: this.separator, headers: this.hasHeader }))
-      .on('header', (header) => {
-        if (this.rows === [] && this.hasHeader === true) {
-          this.schema = generateSchema(header);
-          this.configRows(header);
-        }
-      })
-      .on('data', (csvData: string[]) => {
-        if (this.schema === {} || this.schema === undefined) {
-          setTimeout(() => {
-            if (this.schema === {} || this.schema === undefined) {
-              throw `Error. Timeout, schema not generated`;
-            } else {
-              const obj = csvDataToJSON(this.schema, csvData);
-              this.push(obj);
-            }
-          }, 1000);
-        } else {
-          const obj = csvDataToJSON(this.schema, csvData);
-          this.push(obj);
-        }
+  _transform(data: Buffer, encoding, callback) {
+    const dataString = data.toString();
+    const dataLines = dataString.split(/\r\n|\r|\n/).filter(line => line !== '');
+    if (dataLines && !dataString.match(/\r\n|\r|\n/)) {
+      this.remainder += dataLines.pop();
+    } else {
+      dataLines[0] = this.remainder + dataLines[0];
+      if (!this.headerList) {
+        this.headerList = dataLines.shift().split(this.separator);
+        this.configRows(this.headerList);
+        console.log('Generated schema from first row:');
+        this.printSchema();
+      }
+      const lastChar = dataString[dataString.length - 1];
+      if (lastChar === '\n' || lastChar === '\r') {
+        this.remainder = '';
+      } else {
+        this.remainder = dataLines.pop();
+      }
+      const headerNames = {};
+      this.rows.forEach((row) => {
+        headerNames[row.headerName] = '';
       });
-    })
-    .on('error', (err) => {
-      throw err;
-    });
+      dataLines.forEach((line) => {
+        const values = line.split(this.separator);
+        // Queda implementar ausencia de filas en la configuración
+        const totalColumns = values.length;
+        for (let i = 0; i < totalColumns; i += 1) {
+          headerNames[Object.keys(headerNames)[i]] = values[i];
+        }
+        const obj = csvDataToJSON(this.schema, headerNames);
+        this.push(obj);
+
+      });
+    }
     callback();
+  }
+
+  getDataLines(data: Buffer) {
+
   }
 }
