4774b3f86bc5b0b5c9f49f5deea39d5fee07cf99
Eduardo Soto <esotom92@gmail.com>
2019-02-08 17:21:01 -0300

    + feature: objectParser and test

--- src/JSONparser.ts
+++ src/JSONparser.ts
@@ -1,4 +1,10 @@
 import { tokenizeClassifier } from './tokenClassify';
+import { StringifyOptions } from 'querystring';
+
+interface IObjParseConfig {
+  errorOnNull?: boolean;
+  separator: string;
+}
 
 export function nestingTokenize(obj: object): string[] {
   let pathList = Object.keys(obj);
@@ -38,4 +44,39 @@ export function nestingTokenize(obj: object): string[] {
     objectFlag = false;
   }
   return pathList;
+}
+
+export function objectParser(obj: object, pathHeader: string[], config: IObjParseConfig) {
+  const rowSize = pathHeader.length;
+  let path = obj;
+  let row = '';
+  let separator = ',';
+  if (config) {
+    if (config.separator) {
+      separator = config.separator;
+    }
+  }
+  for (let i = 0; i < rowSize; i += 1) {
+    const nesting = tokenizeClassifier(pathHeader[i]);
+    const nestingSize = nesting.modes.length;
+    for (let j = 0; j < nestingSize; j += 1) {
+      if (path) {
+        if (nesting.modes[j] === 'Object' || nesting.modes[j] === 'Root') {
+          path = path[nesting.tokens[j]];
+        } else if (nesting.modes[j] === 'Array') {
+          path = path[parseInt(nesting.tokens[j], 10)];
+        }
+      } else {
+        if (config.errorOnNull) {
+          throw `Error, null path on ${pathHeader[i]}`;
+        } else {
+          row += `null${separator}`;
+          break;
+        }
+      }
+    }
+    if (path) row += String(path) + separator;
+    path = obj;
+  }
+  return row;
 }
---
+++ test/objectParser.test.ts
@@ -0,0 +1,31 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+import { objectParser, nestingTokenize } from '../src/JSONparser';
+
+const testobj = {
+  Name: 'Eduardo',
+  telephone: [
+    55555,
+    66666,
+  ],
+  Location: {
+    Address: {
+      Street: 'Beale',
+      Number: 99,
+      MailBox: [
+        'Mail',
+        'Boxes',
+        'Amazon',
+      ],
+    },
+    ZipCode: 7341231,
+  },
+};
+
+const headerRow = nestingTokenize(testobj);
+
+describe('Object Parser to row string', () => {
+  it('should return a row string with the object prperties', () => {
+    chai.expect(objectParser(testobj, headerRow, undefined)).to.equal('Eduardo,55555,66666,Beale,99,Mail,Boxes,Amazon,7341231,');
+  });
+});
