53da60a422a3dd6fe837fe811f833656720b930f
Eduardo Soto <esotom92@gmail.com>
2019-02-11 16:44:02 -0300

    + improve: changed testStreaming

--- test/testStreaming.ts
+++ test/testStreaming.ts
@@ -5,13 +5,20 @@ const fs = require('fs');
 const pathCSV = './test/csvFile.csv';
 const c2j = new CSV2JSON(undefined, undefined);
 let str = '';
-console.log('batata');
+let i = 0;
 fs.createReadStream(pathCSV)
 .pipe(c2j)
 .on('data', (data) => {
-  str += JSON.stringify(data, null, 2);
+  str = JSON.stringify(data, null, 2);
+  i += 1;
+  if (i % 10000 === 0) {
+    console.log(str);
+  }
+})
+.on('error', (err) => {
+  console.log(err);
 })
 .on('end', () => {
-  console.log('STRING');
+  console.log('END');
   console.log(str);
 });
